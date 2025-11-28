0119d89f0285979610a50f6daaf526e30e021c14
sakshammaggu <sakshammaggu80@gmail.com>
2025-11-29 00:59:46 +0530

    signup page frontend

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -14,7 +14,7 @@ const Login = ({setCurrentPage}) => {
   }
 
   return (
-    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center ">
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
       <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
       <p className="text-xs text-slate-700 mt-[5px] mb-6">
         Please enter your details to log in
--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,75 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Input from '../../components/inputs/Input'
 
-const SignUp = () => {
+const SignUp = ({setCurrentPage}) => {
+  const [profilePic, setProfilePic] = useState(null)
+  const [fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate()
+
+  const handleSignUp = async(e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>SignUp</div>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create an Account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Join us today by entering your details below.
+      </p>
+
+      <form onSubmit={handleSignUp}>
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
+          <Input
+            value={fullName}
+            onChange={({target}) => setFullName(target.value)}
+            label="Full Name"
+            placeholder="John Doe"
+            type="text"
+          />
+          <Input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john.doe@example.com"
+            type="email"
+          />
+          <Input
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+          />
+        </div>
+
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button
+          type="submit"
+          className="btn-primary"
+        >
+          SIGN UP
+        </button>
+
+        <p className="text-[13px] text-slate-800 mt-3">
+          Already have an account?{" "}
+          <button
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => {
+              setCurrentPage("login");
+            }}
+          >
+            Login
+          </button>
+        </p>
+      </form>
+    </div>
   )
 }
 
