694b18bd506cf260a11f3e36b0f7417b2cf57d77
sakshammaggu <sakshammaggu80@gmail.com>
2025-11-29 01:41:24 +0530

    backend folder setup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.use(express.json());
+
+// Routes
+
+// Serve uploads folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {}));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
