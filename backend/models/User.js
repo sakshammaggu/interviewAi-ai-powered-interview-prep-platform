b28690c99563d82e81671072e89fae18a0ea2710
sakshammaggu <sakshammaggu80@gmail.com>
2025-12-01 19:27:51 +0530

    add mongoose models for User, Session, and Question with schemas and connection for db

---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,       
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImageUrl: {
+        type: String,
+        default: null
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
