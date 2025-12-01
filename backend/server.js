918379bff8c0d5865553a5556a82b277aac94f0d
sakshammaggu <sakshammaggu80@gmail.com>
2025-12-01 20:00:46 +0530

    implement authentication routes and middleware for user registration, login, and profile access

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes")
 
 const app = express();
 
@@ -14,9 +17,17 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+connectDB();
+
 app.use(express.json());
 
 // Routes
+app.use('/api/auth', authRoutes);
+// app.use('/api/sessions', sessionRoutes);
+// app.use('/api/questions', questionRoutes);
+
+// app.use('/api/ai/generate-questions', protect, generateInterviewQuestions);
+// app.use('/api/ai/generate-explanation', protect, generateConceptExplanation);
 
 // Serve uploads folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {}));
